1840b18ca6e968abba1320e3be9c6b0e4c8be5dd
diekus <diekus@gmail.com>
2016-11-24 21:43:05 +0000

    Can load several images sequentially
    TO DO: must be updated to change the texture instead of recreating a
    scene.

--- js/aroundviewer.js
+++ js/aroundviewer.js
@@ -2,10 +2,12 @@ function handleFiles(files){
     if (!files.length) {
         fileList.innerHTML = "<p>No files selected!</p>";
     } else {
+        var placeHolder = document.getElementById('virtualArea');
+        placeHolder.innerHTML= ""; //must update to change the texture instead
         var img = document.createElement('img');
         img.setAttribute('id', 'texture')
         img.src = window.URL.createObjectURL(files[0]);
-        
+        var skys = document.getElementsByTagName('a-sky');
         img.onload = function() {
            
             var sky = document.createElement('a-sky');

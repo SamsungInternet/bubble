0c6d1f2905ef8c607e9ad7badacb9cd220483362
diekus <diekus@gmail.com>
2016-11-24 18:31:25 +0000

    Experimental version
    First "Complete" experimental version.

--- js/aroundviewer.js
+++ js/aroundviewer.js
@@ -1,10 +1,39 @@
-function handleFiles(fileNames){
-    console.log(fileNames);
-    
-    var createdImage = new Image();
-    createdImage.src = fileNames[0].name;
+function handleFiles(files){
+    if (!files.length) {
+        fileList.innerHTML = "<p>No files selected!</p>";
+    } else {
+        var img = document.createElement('img');
+        img.setAttribute('id', 'texture')
+        img.src = window.URL.createObjectURL(files[0]);
+        
+        img.onload = function() {
+           
+            var sky = document.createElement('a-sky');
+            var scene = document.createElement('a-scene');
+            var assets = document.createElement('a-assets');
+            var virtualPlaceholder = document.getElementById('virtualArea');
 
-    var myImage = document.getElementById('myimage');
-    
-    myImage.src = fileNames[0].name;
+            assets.appendChild(img);
+            sky.setAttribute('src', '#texture');
+            scene.appendChild(assets);
+            scene.appendChild(sky);
+            virtualPlaceholder.appendChild(scene);
+
+            window.URL.revokeObjectURL(this.src);
+        }
+        document.body.appendChild(img);
+    }
 }
+
+function init(){
+    var uploadBtn = document.getElementById("selectButton"),
+        fileElem = document.getElementById("fileSelection");
+
+    uploadBtn.addEventListener("click", function (e) {
+        if (fileElem) {
+            fileElem.click();
+        }
+        e.preventDefault(); // prevent navigation to "#"
+        }, false);
+}
+

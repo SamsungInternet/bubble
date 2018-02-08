e2fb39ac881381868af4aa80a52c13f3824d5d07
Diego <diekus@gmail.com>
2018-02-08 14:45:28 +0000

    migrated map to globe!

--- map/js/vr0.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var g_scene = document.querySelector('a-scene');
-
-    g_scene.addEventListener('loaded', function() {
-        console.log('loaded scene');
-        deviceDetection();
-    });    
-});
-
